eb2a60a3c2eed23a40c769229a8992ac21f50bf6
proindian27 <dbbdnaik27@gmail.com>
2025-04-01 23:43:24 +0530

    Update Hero.jsx
    Frontend changes to display only 1 column in output

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
 	const [age, setAge] = useState("");
 	const [displayText, setDisplayText] = useState("Recommendations !!!");
 
-	const handleSubmit = async() => {
+	const handleSubmit = async () => {
 		if (!patientType || !gender || !age) {
 			alert("Please fill in all the fields before submitting.");
 			return;
@@ -18,17 +18,17 @@ function Hero() {
 			return;
 		}
 
-        try {
-            // Send POST request to Flask backend
-            const response = await axios.post("http://localhost:5005/", {
-                type: patientType,
-                specimenType: specimenType,
-                gender: gender,
-                age: age
-            });
+		try {
+			// Send POST request to Flask backend
+			const response = await axios.post("http://localhost:5005/", {
+				type: patientType,
+				specimenType: specimenType,
+				gender: gender,
+				age: age,
+			});
 
-            // Display the prediction result
-            setDisplayText(
+			// Display the prediction result
+			setDisplayText(
 				<div className="overflow-auto max-h-96">
 					<h2 className="text-2xl font-bold mb-4">Prediction Results</h2>
 					<table className="min-w-full bg-white border border-gray-300 ">
@@ -37,27 +37,29 @@ function Hero() {
 								<th className="py-2 px-4 border-b">Antibiotic</th>
 								<th className="py-2 px-4 border-b">Resistance Status</th>
 								<th className="py-2 px-4 border-b">Accuracy (%)</th>
-								<th className="py-2 px-4 border-b">Sensitivity (%)</th>
+								{/* <th className="py-2 px-4 border-b">Sensitivity (%)</th> */}
 							</tr>
 						</thead>
 						<tbody>
 							{response.data.predictions.map((item, index) => (
 								<tr key={index} className="hover:bg-gray-100 text-black">
 									<td className="py-2 px-4 border-b">{item.antibiotic}</td>
-									<td className="py-2 px-4 border-b">{item.resistance_status}</td>
+									<td className="py-2 px-4 border-b">
+										{item.resistance_status}
+									</td>
 									<td className="py-2 px-4 border-b">{item.accuracy}</td>
-									<td className="py-2 px-4 border-b">{item.sensitivity}</td>
+									{/* <td className="py-2 px-4 border-b">{item.sensitivity}</td> */}
 								</tr>
 							))}
 						</tbody>
 					</table>
 				</div>
 			);
-        } catch (error) {
-            console.error("Error making prediction:", error);
-            alert("An error occurred while processing your request.");
-        }
-    };
+		} catch (error) {
+			console.error("Error making prediction:", error);
+			alert("An error occurred while processing your request.");
+		}
+	};
 
 	return (
 		<>
